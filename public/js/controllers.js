3018cdf8c686d0e89a52f11750ffab85e1ed198c
Ting Chen <tingshen.chen@gmail.com>
2016-03-29 16:44:51 -0700

    add hark, which detects whether or not person is speaking or not

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module("SmartScribe.controllers", [])
-.controller('MainCtrl', ["$scope", "LicodeService", function($scope, LicodeService){
+.controller('MainCtrl', ["$scope", "LicodeService", "SpeechRecognition", function($scope, LicodeService, SpeechRecognition){
 	$scope.isChrome = true;
 	if(!("webkitSpeechRecognition" in window)) {
 		$scope.isChrome = false;
