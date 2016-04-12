23f86eae1eb88e7ac0604dedeb09d5e3e3a3b8fd
Ting Chen <tingshen.chen@gmail.com>
2016-04-11 22:02:40 -0700

    Quick front-end deploy

--- public/js/services.js
+++ public/js/services.js
@@ -77,7 +77,7 @@ angular.module("SmartScribe.services", [])
 			 * When a stream no longer exists in the room
 			 */
 			room.addEventListener("stream-removed", function(streamEvent){
-
+				$rootScope.$emit("streamUpdate", stream, false);
 			});
 
 			/**
