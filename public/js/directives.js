4dfb348fded270bf3e851083ae200295b6065487
Ting Chen <tingshen.chen@gmail.com>
2016-04-15 16:57:58 -0700

    Quick front-end deploy

--- public/js/directives.js
+++ public/js/directives.js
@@ -51,19 +51,77 @@ angular.module("SmartScribe.directives", [])
 		restrict: 'A',
 		link: function(scope, element, attrs) {
 			var timer;
+			var containerDims;
+			var numParticipants = element.children().length;
+
+			/**
+			 * Sets the current dimensions 
+			 */
 			function setDimensions() {
-				scope.containerDims = {
+				containerDims = {
 					width: $window.offsetWidth,
 					height: $window.offsetHeight
 				};
+				calculateInnerDimensions();
 			}
-			setDimensions();
 
+			/**
+			 * Calculate the best size to fit the elements 
+			 * in the container while maintaing 16:9 ratio
+			 */
+			function calculateInnerDimensions() {
+
+				var currentRatio = containerDims.width/containerDims.height;
+				switch(numParticipants) {
+					case 2:
+						if(currentRatio > (16*2/9)) {
+							videoDimensions.width = 16*videoDimensions.height/9;
+						} else {
+							videoDimensions.width = videoDimensions.width/2;
+							videoDimensions.height =  (9/16) * videoDimensions.width;
+							
+						}
+					
+						break;
+					case 5:
+					case 6:
+						if(currentRatio > (16*2/9)*(3/2)) {
+							videoDimensions.height = videoDimensions.height/2;
+							videoDimensions.width = 16*videoDimensions.height/9;
+						} else {
+							videoDimensions.width = videoDimensions.width/3;
+							videoDimensions.height = (9/16) * videoDimensions.width;
+						}
+						break;
+					default:
+						// current doc dims are wider than 16:9, use height to constrain
+						if(currentRatio > 16/9) {
+							videoDimensions.width = 16*videoDimensions.height/9;
+						} else {
+							videoDimensions.height = 9*videoDimensions.width/16;
+						}
+						if(numParticipants > 6){
+							videoDimensions.width = videoDimensions.width/3;
+							videoDimensions.height = videoDimensions.height/3;
+						} else if(numParticipants === 3 || numParticipants === 4) {
+							videoDimensions.width = videoDimensions.width/2;
+							videoDimensions.height = videoDimensions.height/2;
+						}
+				}
+			};
+
+			setDimensions();
 			angular.element($window).bind("resize", function(){
 				if(timer){
 					$timeout.cancel(timer);
 				}
 				timer = $timeout(setDimensions, 250);
+			});
+			scope.$on("newParticipant", function(){
+				$timeout(function(){
+					var numParticipants = element.children().length;
+					calculateInnerDimensions();
+				});
 			});
 
 		}
