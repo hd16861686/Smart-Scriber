d7b73ef51f64dc266e7c10891ef210324e31987d
Ting Chen <tingshen.chen@gmail.com>
2016-04-15 17:44:14 -0700

    Quick front-end deploy

--- public/js/directives.js
+++ public/js/directives.js
@@ -61,6 +61,8 @@ angular.module("SmartScribe.directives", [])
 			 * Sets the current dimensions 
 			 */
 			function setDimensions() {
+				videoDimensions.width = "inherit";
+				videoDimensions.height = "inherit";
 				containerDims = {
 					width: element[0].offsetWidth,
 					height: element[0].offsetHeight
