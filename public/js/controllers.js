d202bb46fd836b6e3fd5406efe7e460a3c9f7b8c
Ting Chen <tingshen.chen@gmail.com>
2016-04-18 14:35:38 -0700

    Quick front-end deploy

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -37,7 +37,7 @@ angular.module("SmartScribe.controllers", [])
 	});
 	$interval(function(){
 		if($scope.roomJoined){
-			$scope.allTranscripts.push("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+			$scope.allTranscripts.push({name:"test", time: new Date(), message:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."});
 
 		}
 	}, 2000)
